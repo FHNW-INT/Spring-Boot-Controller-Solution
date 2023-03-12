de020c4f5620b06d06b35deeb6ca83fdbeee198e
charutapande <32811202+charutapande@users.noreply.github.com>
2023-03-12 10:23:30 +0000

    added devcontainer.json

--- pizza/src/main/java/ch/fhnw/pizza/PizzaApplication.java
+++ pizza/src/main/java/ch/fhnw/pizza/PizzaApplication.java
@@ -2,21 +2,21 @@ package ch.fhnw.pizza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-//@RestController
+@RestController
 public class PizzaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PizzaApplication.class, args);
 	}
 
-/* 	@GetMapping(value="/")
+ 	@GetMapping(value="/")
     public String getWelcomeString() {
         
         return "Pizza Takeaway Service, welcome!";
-    } */
+    } 
 
 }
